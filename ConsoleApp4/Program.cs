ab1b8daf19c242b8ba74ac09882bd9baf9f69458
Maksonch1k <123887520+Maksonch1k@users.noreply.github.com>
2024-02-19 09:46:49 +0200

    add

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите документ: HML, DOC, TXT");
+            string format = Console.ReadLine();
+            AbstractHandler handler = null;
+
+            switch (format.ToUpper())
+            {
+                case "XML":
+                    handler = new HMLHandler();
+                    break;
+                case "DOC":
+                    handler = new DOCHandler();
+                    break;
+                case "TXT":
+                    handler = new TXTHandler();
+                    break;
+                default:
+                    Console.WriteLine("Неподерж. формат документа");
+                    return;
+            }
+            handler.Open();
+            handler.Change();
+            handler.Create();
+            handler.Save();
+            Console.ReadLine();
+
+            
+        }
+    }
+}
